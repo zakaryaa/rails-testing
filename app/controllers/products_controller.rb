95e9d0919ce2671dd40015bd83730833d1fbd796
zakarya kartti <kartti.zakarya@gmail.com>
2022-10-18 19:30:18 +0100

    Add Product Hunt list

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+end
